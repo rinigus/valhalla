c1b10aa441cd01315f5002769d01ce2134c477d0
Rinigus <rinigus.git@gmail.com>
2021-01-14 09:04:47 +0200

    cleanup dependency library when composing pc
    For some reason, libboost_filesystem dependency detected on Flatpak has
    library with its version in the path. This trips deplib generation and
    leads to failure to link the server. This commit is expected to fix it

--- cmake/PkgConfig.cmake
+++ cmake/PkgConfig.cmake
@@ -1,3 +1,4 @@
 string(REGEX REPLACE " [^ ]*valhalla[^ ]+" "" deplibs "${deplibs}")
-string(REGEX REPLACE " [^ ]*lib([^/ ]+).so" " -l\\1" deplibs "${deplibs}")
+string(REGEX REPLACE " [^ ]*lib([^/ ]+).so.[a-zA-Z0-9_.-]*" " -l\\1 " deplibs "${deplibs}")
+string(REGEX REPLACE " [^ ]*lib([^/ ]+).so" " -l\\1 " deplibs "${deplibs}")
 configure_file(${INPUT} ${OUTPUT} @ONLY)
